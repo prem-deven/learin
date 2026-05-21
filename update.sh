cb5bf6a8b6e92a0ff7199b236a8fb8b8290e17a1
premdven <219492092+Premdven@users.noreply.github.com>
2026-05-21 19:55:00 +0530

    Feat: Adding update and clean in script

--- update.sh
+++ update.sh
@@ -5,3 +5,8 @@ sudo apt update && sudo apt upgrade
 sudo apt autoremove && sudo apt autoclean
 
 echo "Your system is updated and clean!!"
+
+
+# this file is for learing bash scripts and git commands 
+
+#bash scripts
